3f3ba6d2fe2d9a48f28eddd9509623517742d728
37North <37northapps@gmail.com>
2020-04-10 14:55:28 +0430

    PEP8 compliant, requirement.txt added, and some minor optimizations

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, url_for, redirect, render_template, request, session, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, request, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, Admin
-from werkzeug import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -10,55 +11,45 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 app.secret_key = "development-key"
 
-
-	
-	
-	
-@app.route('/adminLogin', methods = ['GET', 'POST'])
-def adminLogin():
-	AUTH = request.authorization
-	user = Admin.query.filter_by(username = AUTH.username).first()
-
-	if not AUTH or not AUTH.username or not AUTH.password or not user:
-		return make_response('The user or password is wrong!')
-
-	
-	elif check_password_hash(user.pwdhash, AUTH.password):
-
-		return make_response('Hi ' + AUTH.username)
-	else:
-		return make_response('The user or password is wrong!')
-			
-
-
-@app.route('/change_password/<uid>', methods = ['GET', 'POST'])
-def updatePass(uid):
-	new_pass = request.get_json()
-	hash_new_pwd = generate_password_hash(new_pass['pwdhash'])
-	user = Admin.query.filter_by(uid = uid).first()
-	user.pwdhash = hash_new_pwd
-	db.session.commit()
-	return jsonify({'msg':'pass updated'})
-	
-	
-
-
-
-	
-@app.route('/user', methods = ['POST'])
+@app.route('/user', methods=['POST'])
 def create_user():
-	data = request.get_json()
-	hash_psw = generate_password_hash(data['pwdhash'])
-	newUser = Admin(data['username'], hash_psw)
-	db.session.add(newUser)
-	db.session.commit()
-	return jsonify({'msg':'new user created'})
-	
+    '''
+    Register as a new user.
+    '''
+    data = request.get_json()
+    hash_pwd = data['pwdhash']
+    new_user = Admin(data['username'], hash_pwd)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'msg':'new user created'})
 
 
-	
+@app.route('/login', methods=['GET', 'POST'])
+def admin_login():
+    '''
+    Login with your credentials.
+    '''
+    auth = request.authorization
+    user = Admin.query.filter_by(username=auth.username).first()
 
+    if not auth or not auth.username or not auth.password or not user:
+        return make_response('The user or password is wrongg!')
+    elif check_password_hash(user.pwdhash, auth.password):
+        return make_response('Hi ' + auth.username)
+    else:
+        return make_response('Password is wrong for ' + user.username)
 
+@app.route('/change_password/<uid>', methods=['GET', 'POST'])
+def update_pass(uid):
+    '''
+    You may want to change your password.
+    '''
+    new_pass = request.get_json()
+    hash_new_pwd = generate_password_hash(new_pass['pwdhash'])
+    user = Admin.query.filter_by(uid=uid).first()
+    user.pwdhash = hash_new_pwd
+    db.session.commit()
+    return jsonify({'msg':'pass updated'})
 
 if __name__ == "__main__":
     app.run(debug='True', port='4000')
--- models.py
+++ models.py
@@ -3,20 +3,29 @@ from werkzeug import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Admin(db.Model):
-	__tablename__ = 'admin'
-	uid = db.Column(db.Integer, primary_key = True)
-	username = db.Column(db.String(120), unique = True)
-	pwdhash = db.Column(db.String(255))
-	
-	def __init__(self, username, pwdhash):
-		self.username = username.lower()
-		self.set_password(pwdhash)
-	
-	
-	def set_password(self, pwdhash):
-		self.pwdhash = generate_password_hash(pwdhash)
-		
-	def check_password(self, password):
-		return check_password_hash(self.pwdhash, pwdhash)
-		
-		
+    '''
+    Admin class
+    '''
+    __tablename__ = 'admin'
+    uid = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120), unique=True)
+    pwdhash = db.Column(db.String(255))
+
+    def __init__(self, username, pwdhash):
+        '''
+        Define the variables
+        '''
+        self.username = username.lower()
+        self.set_password(pwdhash)
+
+    def set_password(self, pwdhash):
+        '''
+        Set Passwords
+        '''
+        self.pwdhash = generate_password_hash(pwdhash)
+
+    def check_password(self, pwdhash):
+        '''
+        Check the passwords
+        '''
+        return check_password_hash(self.pwdhash, pwdhash)
